9970bf27fe43d76063c7e31cc538bfd0f72b04da
Bukkiee20 <adeyooyebukola22@gmail.com>
2024-01-21 00:48:52 +0100

    Uploaded day8

---
+++ Day7/hangman_words.py
@@ -0,0 +1,6 @@
+word_list = [
+    "abruptly", "bagpipes", "cobweb", "dizzying", "embezzle", "faking", "galaxy", 
+    "hyphen", "ivory", "jigsaw", "kazoo", "luxury", "microwave", "oxygen", 
+    "pajama", "quartz", "rhythm", "sphinx", "topaz", "unknown", "vodka", 
+    "wizard", "xylophone", "yacht", "zealous"
+]
--- hangman_words.py
+++
@@ -1,6 +0,0 @@
-word_list = [
-    "abruptly", "bagpipes", "cobweb", "dizzying", "embezzle", "faking", "galaxy", 
-    "hyphen", "ivory", "jigsaw", "kazoo", "luxury", "microwave", "oxygen", 
-    "pajama", "quartz", "rhythm", "sphinx", "topaz", "unknown", "vodka", 
-    "wizard", "xylophone", "yacht", "zealous"
-]
